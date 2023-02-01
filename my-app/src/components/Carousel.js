ebb387001d21d8090d1766f95902631653770ad4
Jenn Marshall <jennyfever@gmail.com>
2023-02-01 00:00:40 -0700

    fixed particles for react deploy

--- my-app/src/components/Carousel.js
+++ my-app/src/components/Carousel.js
@@ -1,30 +1,30 @@
-import React from "react";
-import bulmaCarousel from "bulma-carousel/dist/js/bulma-carousel.min.js";
+// import React from "react";
+// import bulmaCarousel from "bulma-carousel/dist/js/bulma-carousel.min.js";
 
-class Carousel extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {
-    var carousels = bulmaCarousel.attach(".carousel", {
-      slidesToScroll: 1,
-      slidesToShow: 1,
-    });
-  }
-  render() {
-    return (
-      <div>
-        <div class="carousel carousel-animated carousel-animate-slide">
-          <div class="carousel-container">
-            <div class="carousel-item has-background is-active">
-              <img class="is-background" src="" alt="" width="" height="" />
-              <div class="title">Title</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+// class Carousel extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   componentDidMount() {
+//     var carousels = bulmaCarousel.attach(".carousel", {
+//       slidesToScroll: 1,
+//       slidesToShow: 1,
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <div class="carousel carousel-animated carousel-animate-slide">
+//           <div class="carousel-container">
+//             <div class="carousel-item has-background is-active">
+//               <img class="is-background" src="" alt="" width="" height="" />
+//               <div class="title">Title</div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-export default Carousel;
+// export default Carousel;
